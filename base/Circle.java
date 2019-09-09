331729350340fa6a0a6d9cb667e7eb46e00500e8
SuhEunyeong <eunyeong317@gmail.com>
2019-09-09 14:12:00 +0900

    Circle클래스를 수정했습니다.

--- base/Circle.java
+++ base/Circle.java
@@ -22,4 +22,12 @@ public class Circle implements Shape
     public boolean equals(){
         return(
     }
+    
+    public void draw(){
+        System.out.println("반지름이 " + radius +"인 " + "원입니다.");
+    }
+    
+    public double getArea(){
+        return radius * radius * PI;
+    }
 }
