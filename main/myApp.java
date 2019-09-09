f7b3948f852207be89c005fda5e94e05063dd0f0
BangDaeHo <hanav1@naver.com>
2019-09-09 14:02:23 +0900

    myApp클래스 16번째줄 수정 및 base패키지 내의 Circle클래스와 연결

--- main/myApp.java
+++ main/myApp.java
@@ -1,19 +1,19 @@
 package main;
-
+import base.Circle;
 /**
  * Write a description of class myApp here.
  *
  * @author ()
  * @version (a version number or a date)
  */
-public class myApp
+public class myApp extends Circle
 {
     public static void main(String[] args){
         Circle c1 = new Circle(2, 3, 5);
         Circle c2 = new Circle(2, 3, 10);
         System.out.println("원1 : " + c1);
         System.out.println("원2 : " + c2);
-        if()
+        if(c1.equals(c2))
             System.out.println("같은 원");
         else
         System.out.println("서로 다른 원");
