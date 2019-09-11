38bc51d056ce16c24e3841223c37df03c7402586
BangDaeHo <hanava1@naver.com>
2019-09-11 14:33:41 +0900

    Circle클래스내 중복 메소드 삭제

--- base/Circle.java
+++ base/Circle.java
@@ -33,12 +33,4 @@ public class Circle implements Shape
     public double getArea(){
         return radius * radius * PI;
     }
-    
-    public void draw(){
-        System.out.println("반지름이 " + radius +"인 " + "원입니다.");
-    }
-    
-    public double getArea(){
-        return radius * radius * PI;
-    }
 }
